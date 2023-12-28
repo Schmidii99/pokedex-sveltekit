d29e4fef229926537536c9aebd811fc2a85e1256
Florian Schmid <melonencool@gmail.com>
2023-12-28 12:43:59 +0100

    fetch data from api (SSR)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import type { PageData } from "./$types.js";
+    import { generations } from "./generations";
+
+    export let data: PageData;
+</script>
+
+{#each data.monsters as monster (monster.url)}
+    <p>{monster.id}: {monster.name}</p>
+{/each}
+
+{#each generations as generation (generation.id)}
+    <h1>{generation.name}</h1>
+    <p>Games: {generation.games.join(", ")}</p>
+    <p>Main Region: {generation.main_region}</p>
+{/each}
