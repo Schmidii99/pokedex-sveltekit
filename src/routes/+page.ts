d48e3d2a048b507aaa19db963b6f400fe13b37d7
Florian Schmid <melonencool@gmail.com>
2023-12-29 00:49:34 +0100

    add types

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,17 +1,22 @@
 import type { PageLoad } from "./$types";
 
-type IndexMonster = {
+type ApiMonster = {
     name: string,
     url: string
 }
 
+export type IndexMonster = ApiMonster & {
+    id: number,
+    image: string
+}
+
 export const load = (async ({ fetch }) => {    
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1000");
     const json = await response.json();
 
-    const monsters = json.results.map((monster: IndexMonster) => {
-        const splitUrl = monster.url.split("/");
-        const id = splitUrl[splitUrl.length - 2];
+    const monsters: IndexMonster[] = json.results.map((monster: ApiMonster) => {
+        const splitUrl: string[] = monster.url.split("/");
+        const id: number = Number(splitUrl[splitUrl.length - 2]);
         return {
             name: monster.name,
             url: monster.url,
