4bbd8c6f01b69ad59252a69914d1ca0f0fbeff5e
Florian Schmid <melonencool@gmail.com>
2023-12-29 17:50:34 +0100

    add navbar with +layout.svelte and my-monster route

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script>
+    import { page } from "$app/stores";
+
+</script>
+
+<div class="navbar">
+    <a href="/" class:active={$page.url.pathname == "/"}>
+        Home
+    </a>
+    <a href="/my-monsters/" class:active={$page.url.pathname == "/my-monsters"}>
+        My Monsters
+    </a>
+</div>
+
+<div class="container">
+    <slot />
+</div>
+
+<style>
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+        background-color: #333;
+        color: #fff;
+        padding: 10px;
+    }
+    .navbar a {
+       color: #fff;
+       text-decoration: none;
+       padding: 4px;
+    }
+    .navbar a.active {
+       color: rgb(32, 136, 239);
+    }
+    .navbar a:hover {
+        color: rgb(32, 136, 239);
+    }
+
+    :global(body) {
+        margin: 0;
+    }
+    .container {
+        margin: 0 auto;
+        max-width: 90%;
+        width: 1200px;
+    }
+</style>
